2f77ef2349f35f220f72c15e783ddafa8d02813e
Paweł Staniucha <pawelstaniucha@wp.pl>
2021-10-13 18:18:46 +0200

    Palindrom2

--- palindromy.py
+++ palindromy.py
@@ -1,6 +1,9 @@
-palindrom = input("Podaj palindrom: ")
-letters = palindrom.replace(" ","")[::-1]
-if palindrom.replace(" ","").lower() == letters.lower():
+palindrom = '"U Gorgon" je loda Ikar. Bawi po balu car. Da łowy. Na lwicę troglodyta i "Rak", co ma moc kariatyd. Olgo - rtęci! Wlany woła Dracula, bo piwa braki. A dolej no grogu!'
+
+palindrom_without_special_chars = ''.join(e for e in palindrom if e.isalnum())
+
+letters = palindrom_without_special_chars.replace(" ","")[::-1]
+if palindrom_without_special_chars.replace(" ","").lower() == letters.lower():
     print ("To jest palindrom")
 else:
     print("Nie jest to palindrom")
